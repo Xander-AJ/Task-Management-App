912638ac6d9420087fd07ae6a1985013acdab28e
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-05-11 10:59:02 +0300

    provides a centralized location for managing and accessing various assets used throughout the application

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+    images: {
+      logoDark: require('./images/Logo-removebg-preview.png'),
+      appIcon: require('./images/Logo.png')
+    },
+    colors: {
+      secondary: '#292929'
+    }
+  }
+  
+  export default assets
